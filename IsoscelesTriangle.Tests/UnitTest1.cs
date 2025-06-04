6336a8e71c1d36cf2a75ea19b95bb834c966a6bf
Wesley <wesley@example.com>
2025-06-04 09:30:02 -0400

    test: added test to check for valid Isosceles Triangle

--- IsoscelesTriangle.Tests/UnitTest1.cs
+++ IsoscelesTriangle.Tests/UnitTest1.cs
@@ -8,8 +8,19 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
+    public void ValidIsoscelesTriangle_Input60and60and60_OutputValidIsoscelesTriangle()
     {
-        Assert.Pass();
+        //Arrange
+        int firstAngle = 60;
+        int secondAngle = 60;
+        int thirdAngle = 90;
+
+        string expected = "This is a valid Isosceles Triangle";
+
+        //Act
+        string actual = IsoscelesTriangle.Services.IsoscelesTriangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+        //Assert
+        Assert.That(actual, Is.EqualTo(expected));
     }
 }
