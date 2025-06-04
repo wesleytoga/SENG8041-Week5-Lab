0976b693340b77a575c7c3bc78213a74ec6abb29
Wesley <wesley@example.com>
2025-06-04 09:22:22 -0400

    feat: added ValidIsoscelesTriangle method

--- IsoscelesTriangle/Class1.cs
+++ IsoscelesTriangle/Class1.cs
@@ -1,6 +1,21 @@
-﻿namespace IsoscelesTriangle;
+﻿namespace IsoscelesTriangle.Services;
 
-public class Class1
+public class IsoscelesTriangle
 {
+    public static string ValidIsoscelesTriangle(int firstAngle, int secondAngle, int
+            thirdAngle)
+    {
+        string result;
 
+        if ((firstAngle == secondAngle) || (firstAngle == thirdAngle) || (secondAngle == thirdAngle))
+        {
+            result = "This is a valid Isosceles Triangle";
+        }
+        else
+        {
+            result = "This is NOT a valid Isosceles Triangle";
+        }
+
+        return result;
+    }
 }
